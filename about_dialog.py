e35bd1e0b8aca3b8c1b9af477359822c448649e6
Peter <szpeto12@gmail.com>
2025-06-04 15:22:11 +0200

    Version v0.7.2

--- about_dialog.py
+++ about_dialog.py
@@ -6,22 +6,31 @@ class AboutDialog(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("About expense manager app")
-        self.setMinimumSize(600, 400)
+        self.setMinimumSize(800, 600)
 
         v_box_layout = QVBoxLayout()
 
         text = QTextEdit()
         text.setReadOnly(True)
 
-        about_text = (
-            "📊 Simple expense manager app created by Peter Szepesi, Slovakia\n\n"
-            "If you like my app and you feel that way, you can support me in my work,\n"
-            "I would be really grateful and appreciate any amount of donation.\n"
-            "You can do it via support me button, or IBAN account transfer,\n"
-            "IBAN account number : SK90 0900 0000 0051 2726 1825"
-        )
+        about_text = ("""
+            <h2>📊 Simple expense manager app created by Peter Szepesi, Slovakia</h2><br>
+            <p>&nbsp;• You can sort by any column by simply clicking the column name, clicking multiple times<br>
+               &nbsp;&nbsp;&nbsp;&nbsp;changes the order, either ascending or descending<br>
+               &nbsp;• For editing any values, double-click any cell, than after fixing the value, press enter to<br>
+               &nbsp;&nbsp;&nbsp;&nbsp;to save the new value<br><br> 
+            If you encounter any issues related to this app, please report them via : szpeto12@gmail.com<br>
+            and if you could, please include the log file, which is located in the same directory,<br>
+            where your .exe file is located, under Log\\log.txt.<br><br>
+            If you like my app and you feel that way, you can support me in my work,<br>
+            I would be really grateful and appreciate any amount of donation.<br>
+            You can do it via support me button, or IBAN account transfer,<br>
+            IBAN account number : SK90 0900 0000 0051 2726 1825<br><br>
+            Thank you very much!<br>
+            Peter</p>
+        """)
 
-        text.setPlainText(about_text)
+        text.setHtml(about_text)
         v_box_layout.addWidget(text)
         self.setLayout(v_box_layout)
 
